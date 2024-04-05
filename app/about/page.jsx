a0c752edee6c05814db86f8b7e600e8d1537c252
haybuka <paschal.nduleo@gmail.com>
2024-04-05 09:51:45 +0100

    meta reset

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -42,12 +42,6 @@ const about = [
   },
 ];
 
-export const metadata = {
-  title: 'IVR | About',
-  description:
-    'Ivr power works, a fast growing system of electrical engineering services. ',
-};
-
 const Index = () => {
   return (
     <main className="mt-20 md:px-0">
--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -2,12 +2,6 @@ import React from 'react';
 import Banner from './banner';
 import Quote from './quote';
 
-export const metadata = {
-  title: 'IVR | Contact',
-  description:
-    'Ivr power works, a fast growing system of electrical engineering services. ',
-};
-
 const Index = () => {
   return (
     <section className="mt-20 md:px-0">
