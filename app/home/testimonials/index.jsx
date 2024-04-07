8758d45241cf98dc2fc7ce04ccbc4ebe5c43d072
haybuka <paschal.nduleo@gmail.com>
2024-04-07 05:47:47 +0100

    testimonial card

--- app/home/testimonials/index.jsx
+++ app/home/testimonials/index.jsx
@@ -2,20 +2,37 @@
 import React, { useCallback, useEffect } from 'react';
 import { useAnimate, useInView, motion, useTime } from 'framer-motion';
 const Testimonials = () => {
-  const time = useTime();
-  const [scope, animate] = useAnimate(time);
+  const [scope, animate] = useAnimate();
   const isInView = useInView(scope);
 
-  console.log({ time });
-
   const handleAnimateBoxCallback = useCallback(async () => {
-    await animate('#boxTwo', { x: 0 }, { duration: 0.5, delay: 5 });
-    await animate('#boxThree', { x: 0 }, { duration: 0.5, delay: 5 });
-  }, [animate]);
-
-  const handleAnimateProgressCallback = useCallback(async () => {
-    await animate('#boxTwo', { x: 0 }, { duration: 0.5, delay: 5 });
-    await animate('#boxThree', { x: 0 }, { duration: 0.5, delay: 5 });
+    await animate('#progressOne', { width: '100%' }, { duration: 5 });
+    await animate('#boxTwo', { x: 0, scale: 1 }, { duration: 0.5, delay: 0.2 });
+    await animate(
+      '#progressTwo',
+      { width: '100%' },
+      { duration: 5, delay: 0.5 }
+    );
+    await animate(
+      '#boxThree',
+      { x: 0, scale: 1 },
+      { duration: 0.5, delay: 0.2 }
+    );
+    await animate(
+      '#progressThree',
+      { width: '100%' },
+      { duration: 5, delay: 0.5 }
+    );
+    await animate(
+      '#boxFour',
+      { x: 0, scale: 1 },
+      { duration: 0.5, delay: 0.2 }
+    );
+    await animate(
+      '#progressFour',
+      { width: '100%' },
+      { duration: 5, delay: 0.5 }
+    );
   }, [animate]);
 
   useEffect(() => {
@@ -25,8 +42,11 @@ const Testimonials = () => {
   }, [isInView, handleAnimateBoxCallback]);
 
   return (
-    <section className="grid grid-cols-12 my-12 bg-red-500">
-      <aside className="col-span-6 flex justify-center items-center p-24">
+    <section
+      className="lg:grid grid-cols-12 my-12 overflow-x-hidden"
+      ref={scope}
+    >
+      <aside className="col-span-7 flex justify-center items-center p-4 md:p-16">
         <div>
           <h3 className="text-4xl font-semibold mb-4">
             What our customers think
@@ -36,58 +56,91 @@ const Testimonials = () => {
             commodi sint, similique cupiditate possimus suscipit delectus illum
             eos iure magnam!
           </p>
-          <ul className="flex gap-x-4">
-            <li className="w-24 h-1 bg-slate-200"> </li>
-            <li className="w-24 h-1 bg-slate-200"> </li>
-            <li className="w-24 h-1 bg-slate-200"> </li>
-            <li className="w-24 h-1 bg-slate-200"> </li>
+          <ul className="flex gap-x-4 my-4">
+            <li className="w-24 h-1 bg-slate-200 relative">
+              {' '}
+              <motion.span
+                initial={{ width: 0 }}
+                id="progressOne"
+                className="h-full bg-black absolute top-0 left-0"
+              ></motion.span>{' '}
+            </li>
+            <li className="w-24 h-1 bg-slate-200 relative">
+              {' '}
+              <motion.span
+                initial={{ width: 0 }}
+                id="progressTwo"
+                className="h-full bg-black absolute top-0 left-0"
+              ></motion.span>{' '}
+            </li>
+            <li className="w-24 h-1 bg-slate-200 relative">
+              {' '}
+              <motion.span
+                initial={{ width: 0 }}
+                id="progressThree"
+                className="h-full bg-black absolute top-0 left-0"
+              ></motion.span>{' '}
+            </li>
+            <li className="w-24 h-1 bg-slate-200 relative">
+              {' '}
+              <motion.span
+                initial={{ width: 0 }}
+                id="progressFour"
+                className="h-full bg-black absolute"
+              ></motion.span>{' '}
+            </li>
           </ul>
         </div>
       </aside>
-      <aside className="col-span-6 over">
+      <aside className="col-span-5 px-8 md:px-10 py-28 lg:py-40 lg:px-0">
         <ul
           ref={scope}
-          className="h-[400px] w-[400px] relative overflow-hidden"
+          className="h-[400px] w-full lg:h-[500px] lg:w-[600px] flex justify-center items-center relative shadow-md   "
         >
           <motion.li
             id="boxOne"
-            className="h-full w-full bg-black absolute top-0 "
+            className="h-full translate-x-0 w-full lg:w-[600px] bg-black absolute top-0 shadow-md "
             initial={{
               x: 0,
             }}
             transition={{
               ease: 'easeIn',
             }}
-          >
-            {' '}
-            First list item
-          </motion.li>
+          ></motion.li>
           <motion.li
             id="boxTwo"
             initial={{
-              x: 300,
+              x: 550,
+              scale: 1.1,
             }}
             transition={{
               ease: 'easeIn',
             }}
-            className="h-full w-full bg-white text-red-500 absolute top-0 right-0 z-10 "
-          >
-            {' '}
-            second list item
-          </motion.li>
+            className="h-full w-full translate-x-0 lg:w-[600px] bg-white  absolute top-0 shadow-md border right-0 z-10 "
+          ></motion.li>
           <motion.li
             initial={{
-              x: 360,
+              x: 580,
+              scale: 1.15,
             }}
+            viewport={{}}
             transition={{
               ease: 'easeIn',
             }}
             id="boxThree"
-            className="h-full w-full bg-green-400 text-white absolute top-0 right-0 z-20"
-          >
-            {' '}
-            third list item
-          </motion.li>
+            className="h-full w-full translate-x-0 lg:w-[600px] bg-black absolute top-0 shadow-md right-0 z-20"
+          ></motion.li>
+          <motion.li
+            initial={{
+              x: 610,
+              scale: 1.2,
+            }}
+            transition={{
+              ease: 'easeIn',
+            }}
+            id="boxFour"
+            className="h-full w-full translate-x-0 lg:w-[600px] bg-red-500 text-white absolute top-0 shadow-md right-0 z-20"
+          ></motion.li>
           <li />
           <li />
         </ul>
