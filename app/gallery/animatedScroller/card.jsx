0713f696fa9040d2b731ea5ed82a17c4e5aaf66e
haybuka <paschal.nduleo@gmail.com>
2024-04-09 14:18:32 +0100

    slider dummy images

--- app/gallery/animatedScroller/card.jsx
+++ app/gallery/animatedScroller/card.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
-const AnimatedCard = () => {
+const AnimatedCard = ({ card }) => {
+  const { image, title } = card;
   return (
-    <aside className="relative h-[400px] w-[300px] bg-ivr-100 flex items-end py-10 px-4">
+    <aside
+      className="relative h-[400px] w-[300px] bg-ivr-100 flex items-end py-10 px-4"
+      style={{
+        backgroundImage: `url(${image})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
       <div>
-        <h3 className="capitalize text-xl text-white font-semibold">
-          Title One
-        </h3>
-        <p className="text-gray-400">Subtitle one</p>
+        <h3 className="capitalize text-xl font-semibold text-white">{title}</h3>
+        <p className="text-gray-400 text-sm">Subtitle one</p>
       </div>
     </aside>
   );
--- app/gallery/animatedScroller/index.jsx
+++ app/gallery/animatedScroller/index.jsx
@@ -7,16 +7,43 @@ const AnimatedScroller = () => {
   const targetRef = useRef(null);
   const { scrollYProgress } = useScroll({ target: targetRef });
   const x = useTransform(scrollYProgress, [0, 1], ['1%', '-95%']);
-  console.log({ x });
+  const cards = [
+    {
+      image: '/about/manWire.jpg',
+      title: 'project one',
+    },
+    {
+      image: '/home/ceo.jpeg',
+      title: 'project two',
+    },
+    {
+      image: '/home/multimeter.jpg',
+      title: 'project three',
+    },
+    {
+      image: '/about/manWire.jpg',
+      title: 'project four',
+    },
+    {
+      image: '/home/grid_two.jpg',
+      title: 'project five',
+    },
+    {
+      image: '/about/manWire.jpg',
+      title: 'project six',
+    },
+    {
+      image: '/home/grid_one.jpg',
+      title: 'project seven',
+    },
+  ];
   return (
     <section ref={targetRef} className="relative h-[300vh]  my-20">
       <div className="h-screen  sticky top-0 flex items-center overflow-hidden">
-        <motion.div style={{ x }} className="flex gap-4 bg-yellow-500">
-          <AnimatedCard />
-          <AnimatedCard />
-          <AnimatedCard />
-          <AnimatedCard />
-          <AnimatedCard />
+        <motion.div style={{ x }} className="flex gap-4 ">
+          {cards.map((card, id) => (
+            <AnimatedCard card={card} key={id} />
+          ))}
         </motion.div>
       </div>
     </section>
